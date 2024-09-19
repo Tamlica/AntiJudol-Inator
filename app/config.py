170ffb0b7652c8bbaa7cdddd726bf5ae739ad909
tamlica <tamlica@tlab.co.id>
2024-09-19 15:28:20 +0700

    add: add env file

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'c29jaWFidXp6LWZpbHRlcmluZy13b3JkLWJ5LXByb2plY3QtZG8=')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'cHJvamVjdC1kby1zb2NpYWJ1enotZmlsdGVyaW5nLXdvcmQtand0')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:757377@localhost/sociabuzz_filter'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
